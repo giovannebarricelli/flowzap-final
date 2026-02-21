c43358a8154141ac4490ab1be374bb8b5b7994b2
giovannebarricelli <giovannebarricellivml@gmail.com>
2026-02-21 01:30:39 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,25 +1,16 @@
-const { Client, LocalAuth } = require('whatsapp-web.js');
-const qrcode = require('qrcode-terminal');
-const express = require('express');
-const app = express();
-
 const client = new Client({
     authStrategy: new LocalAuth(),
     puppeteer: { 
-        headless: true, 
-        args: ['--no-sandbox', '--disable-setuid-sandbox', '--disable-dev-shm-usage'] 
+        headless: true,
+        executablePath: '/usr/bin/google-chrome-stable',
+        args: [
+            '--no-sandbox',
+            '--disable-setuid-sandbox',
+            '--disable-dev-shm-usage',
+            '--disable-gpu',
+            '--no-first-run',
+            '--no-zygote',
+            '--single-process'
+        ]
     }
 });
-
-client.on('qr', (qr) => {
-    qrcode.generate(qr, {small: true});
-    console.log('--- ESCANEIE O QR CODE ABAIXO ---');
-});
-
-client.on('ready', () => console.log('SISTEMA ONLINE E CONECTADO!'));
-
-app.get('/', (req, res) => res.send('Motor Online'));
-const PORT = process.env.PORT || 10000;
-app.listen(PORT, "0.0.0.0", () => console.log(`Rodando na porta ${PORT}`));
-
-client.initialize();
